8683f7928f1d299b73c6ba804024427877f86c53
nuviba <nuriaviba93@hotmail.com>
2021-10-22 18:26:23 +0200

    añadiendo volteado de cartas

--- index.js
+++ index.js
@@ -19,17 +19,36 @@ function mostrarTablero (){
 //función para crear las cartas individuales
 function templateCard(id, objeto){
     
-    document.getElementById("tablero").innerHTML+="<div class=card id="+id+"></div>";
+    document.getElementById("tablero").innerHTML+=`
+    <div class=card  id=${id}>
+        <div class=front >
+            <img src=${objeto.image} onclick="girarCarta(${id})" alt="zelda objet">
+        </div>
+        <div class="back">
+        <img src="https://i.pinimg.com/originals/1a/58/3f/1a583fac90a845c9103e66f10ca9f19b.jpg" onclick="girarCarta(${id})" alt="">
+        </div>
+    </div>
+   `;
     document.getElementById("tablero").style.margin="10px";
     document.getElementById("tablero").style.marginBottom="10px";
-    document.getElementById(id).innerHTML+=`<img src=${objeto.image} alt="zelda objet">`;
-
 }
-
  //generamos las posiciones en el tablero duplicando las cartas y de forma aleatoria
 function genPosRan(){
     let arrayOriginal = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7];
     arrayOriginal.sort(function() { return Math.random() - 0.5 });
     return(arrayOriginal)
 }
+
+function resetTablero(){
+    document.getElementById("tablero").innerHTML="";
+}
+
+function girarCarta (id){
+    console.log("hola")
+    document.getElementById(id).classList.toggle("flipCard");
+}
+
+
+
+
 
